f854903e52869b4619ece7bc9f51772d8e2533a2
yanina <yan.a.pe@hotmail.com>
2025-03-31 19:43:35 -0300

    correccion ejercicio 1 y 2

--- 01_adivina.php
+++ 01_adivina.php
@@ -3,16 +3,22 @@
 	<body>
 		<?
 			$num = $_POST['valor1'];
-			$adivina = rand(1,20);
-			if($num == $adivina)
-            {  
+			
+			if($num>=1 && $num<=20){ 
+				$adivina = rand(1,20);
+				if($num == $adivina)
+				{  
+					echo "ganaste!";
+				}
+				else 
+				{
+					echo "perdiste! <br>";
+					echo "el numero era ".$adivina;
+				}
+			}else 
+			{
+				echo "el  numero ingresado no esta en el rango indicado";
 				
-                echo "ganaste!";
-            }
-            else 
-            {
-                echo "perdiste! <br>";
-				echo "el numero era ".$adivina;
 			}
 		?>
 	<body>
--- 02_ej0002.php
+++ 02_ej0002.php
@@ -1,22 +1,19 @@
-<html><head><title> suma y resta  </title>
-</head>
+<html>
+	<head>
+		<title> suma y resta  </title>
+	</head>
 <body>
-<?php 
-
-   
-if(isset($_POST['check1']) && $_POST['check1'] == 'suma')
-{
-	$sumar=$_POST['valor1'] + $_POST['valor2'];
-	echo "la suma es ".$sumar ;
-	
-}
- if(isset($_POST['check2']) && $_POST['check2'] == 'resta')
-{
-		 $restar= $_POST['valor1'] - $_POST['valor2'];
-		 echo "la resta es ".$restar ;	 
-}
- 
- 
- ?>
+	<?php 
+		if(isset($_POST['operacion']) && $_POST['operacion'] == 'suma')
+		{
+			$sumar=$_POST['valor1'] + $_POST['valor2'];
+			echo "la suma es ".$sumar ;
+		}
+		if(isset($_POST['operacion']) && $_POST['operacion'] == 'resta')
+		{
+			$restar= $_POST['valor1'] - $_POST['valor2'];
+			echo "la resta es ".$restar ;	 
+		}
+		?>
 </body>
 </html>
