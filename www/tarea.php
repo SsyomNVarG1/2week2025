96e43b5bbb127080b114ee3bcb7cea742740557f
ZerathMagic <leoon22estja@gmail.com>
2025-04-01 17:00:34 -0300

    v1.102

--- www/tarea.php
+++ www/tarea.php
@@ -9,11 +9,13 @@ $nac = $_POST['nacimiento'];
 
 $dif = 2025- $nac;
 
-#validacion de edad meto aca.
 
 if( $dif >= 18){
     echo("Eres mayor de edad por lo tanto tus datos fueron  guardados");
     #No se guardar datos asi que le consulto al profe, pero la idea es esta
+
+    
+
 }
 else{
 echo("Los datos no seran guardados debido a la minoria de edad del usuario, adios. seras expulsado de la pagina");
