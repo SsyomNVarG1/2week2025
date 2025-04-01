79426484bf9a82fdaeede2697e6d03ada6b0f12a
ZerathMagic <leoon22estja@gmail.com>
2025-04-01 05:10:32 -0300

    v1.094

--- www/tarea.php
+++ www/tarea.php
@@ -12,6 +12,7 @@ $dif = 2025- $nac;
 #validacion de edad meto aca.
 
 if( $dif >= 18){
+    
     echo("Eres mayor de edad por lo tanto tus datos fueron  guardados");
     #No se guardar datos asi que le consulto al profe, pero la idea es esta
 }
